75d8082715c6f9d4a330e0154b6cafad1423c0de
JessToni <jcmosquera@up.edu.ph>
2025-05-27 23:22:51 +0800

    Setup: product model and product collection in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,23 @@ dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    res.send("Server is ready");
-});
+app.post("/products", async (req, res) => {
+    const product = req.body; // user will send this data
 
-console.log(process.env.MONGO_URI);
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, mesage: "Please provide all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in Create product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+});
 
 app.listen(5000, () => {
     connectDB();
